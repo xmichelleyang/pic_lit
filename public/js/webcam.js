06c350dc0113d27ec5d303e4c3e7611c9ab66079
Albert Lo <ajlo@ucsd.edu>
2019-04-06 17:39:55 -0700

    some stuff

--- public/js/webcam.js
+++ public/js/webcam.js
@@ -12,11 +12,10 @@ captureButton.addEventListener('click', () => {
   context.drawImage(player, 0, 0, canvas.width, canvas.height);
   // Stop all video streams.
   player.srcObject.getVideoTracks().forEach(track => track.stop());
-  modal.style.display = "none";
   canvas.style.display = "block";
-  btn.style.display = "none";
-  var img = canvas.toDataURL("image/png");
-  document.write('<img src="'+img+'"/>');
+  player.style.display = "none";
+  //var img = canvas.toDataURL("image/png");
+  //document.write('<img src="'+img+'"/>');
 });
 
 navigator.mediaDevices.getUserMedia(constraints)
@@ -24,29 +23,3 @@ navigator.mediaDevices.getUserMedia(constraints)
     // Attach the video stream to the video element and autoplay.
     player.srcObject = stream;
 });
-
-// Get the modal
-const modal = document.getElementById('div1');
-
-// Get the button that opens the modal
-const btn = document.getElementById("capWeb");
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
