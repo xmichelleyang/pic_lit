d985088cf63d03e4acd605993ad6a9471e474f5e
Michelle Yang <zomgitsmichelle@gmail.com>
2019-04-06 18:17:03 -0700

    Text output.

--- routes/index.js
+++ routes/index.js
@@ -3,81 +3,51 @@
  * GET home page.
  */
 
-exports.view = function(req, res){
-  // // Imports the Google Cloud client library
+exports.view = async function(req, res){
+
   // const vision = require('@google-cloud/vision');
   //
   // // Creates a client
   // const client = new vision.ImageAnnotatorClient();
   //
   // // Performs label detection on the image file
-  // const [result] = await client.labelDetection('gao.png');
-  // const labels = result.labelAnnotations;
-  // console.log('Labels:');
-  // labels.forEach(label => console.log(label.description));
+  // client
+  //   .labelDetection('gao.png')
+  //   .then(results => {
+  //     const labels = results[0].labelAnnotations;
+  //
+  //     console.log('Labels:');
+  //     labels.forEach(label => console.log(label.description));
+  //   })
+  //   .catch(err => {
+  //     console.error('ERROR:', err);
+  //   });
 
-  // Imports the Google Cloud client library
+  // Imports the Google Cloud client libraries
   const vision = require('@google-cloud/vision');
 
   // Creates a client
   const client = new vision.ImageAnnotatorClient();
 
-  // Performs label detection on the image file
-  client
-    .labelDetection('gao.png')
-    .then(results => {
-      const labels = results[0].labelAnnotations;
+  /**
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+  // const bucketName = 'Bucket where the file resides, e.g. my-bucket';
+  const fileName = '2.jpg';
 
-      console.log('Labels:');
-      labels.forEach(label => console.log(label.description));
-    })
-    .catch(err => {
-      console.error('ERROR:', err);
-    });
-
-
-  // // Imports the Google Cloud client library.
-  // const {Storage} = require('@google-cloud/storage');
-  //
-  // // Instantiates a client. If you don't specify credentials when constructing
-  // // the client, the client library will look for credentials in the
-  // // environment.
-  // const storage = new Storage();
-  //
-  // // Makes an authenticated API request.
-  // storage
-  //   .getBuckets()
-  //   .then((results) => {
-  //     const buckets = results[0];
-  //
-  //     console.log('Buckets:');
-  //     buckets.forEach((bucket) => {
-  //       console.log(bucket.name);
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     console.error('ERROR:', err);
-  //   });
-
+  // Performs text detection on the gcs file
+  const [result] = await client.textDetection(fileName);
+  const detections = result.textAnnotations;
+  console.log('Text:');
+  console.log(detections)
+  // for each ( in detections) {
+  //   console.log()
+  // }
+  // detections.forEach(text => console.log(text['description']));
 
   res.render('index');
 };
 
-// exports.compute = async function quickstart(req, res) {
-//   // Imports the Google Cloud client library
-//   const vision = require('@google-cloud/vision');
-//
-//   // Creates a client
-//   const client = new vision.ImageAnnotatorClient();
-//
-//   // Performs label detection on the image file
-//   const [result] = await client.labelDetection('./resources/wakeupcat.jpg');
-//   const labels = result.labelAnnotations;
-//   console.log('Labels:');
-//   labels.forEach(label => console.log(label.description));
-//
-//
-// };
 exports.webcam = function(req, res){
 	res.render('webcam');
 };
